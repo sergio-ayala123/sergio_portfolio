405750677f136023182cab011881fa34bb7fd9a4
sergio-ayala123 <venturian6@gmail.com>
2022-09-15 19:11:53 -0600

    Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Projects from './pages/Projects';
+import { Box, Container } from '@mui/material';
 
 function App() {
   return (
-    <h1>Start</h1>
+    < div id = 'top'>
+      <NavBar/>
+
+      <Container id = 'about' sx={{ width: 'auto', height: 900, bgcolor: 'white', justifyContent: 'center'}}>
+        <h1> About
+        </h1>
+      </Container>
+
+
+      <Container id = 'projects' sx={{ width: 'auto', height: 900, bgcolor: 'white', justifyContent: 'center' }}>
+        <h1>Projects</h1>
+      </Container>
+
+      <Container id = 'contact' sx={{ width: 'auto', height: 900, bgcolor: 'white', justifyContent: 'center' }}>
+        <h1>Contact</h1>
+      </Container>
+    </div>
+
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import { AppBar, Button, IconButton, Menu, MenuItem, MenuList, Stack, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll"
+const NavBar = () => {
+    return (
+        <>
+            <AppBar position="sticky" style={{ backgroundColor: '#2E82B3' }}>
+                <Toolbar>
+
+                    <Typography variant='h4' style={{ flexGrow: 1 }}>
+                        Sergio Ayala
+                    </Typography>
+
+                    <Stack direction='row' spacing={2}>
+                        <Typography>
+                            <LinkScroll offset = {-100} smooth spy to='about' duration={300}>
+                                <Button style={{ color: 'white' }}> <Typography variant='h5' textTransform={'capitalize'}>About</Typography></Button>
+                            </LinkScroll>
+                            <LinkScroll offset = {-100} smooth spy to='projects' duration={300}>
+                                <Button style={{ color: 'white' }}> <Typography variant='h5' textTransform={'capitalize'}>Projects</Typography></Button>
+                            </LinkScroll>
+
+                            <LinkScroll offset = {-100} smooth spy to='contact' duration={300}>
+
+                                <Button style={{ color: 'white' }}><Typography variant='h5' textTransform={'capitalize'}>Contact</Typography></Button>
+                            </LinkScroll>
+                        </Typography>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+
+export default NavBar;

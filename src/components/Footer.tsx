19f36202af09aac7385c0400bd9498939d3d5dbb
sergio-ayala123 <venturian6@gmail.com>
2022-10-07 20:28:37 -0600

    fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,13 +11,13 @@ const Footer = () => {
                 <a href = "https://www.linkedin.com/in/sergio-ayala-166066200/">
 
 
-                <img src = {linkedIn} height= '100px' width = '100px' style={{borderRadius:'20%'}}></img>
+                <img src = {linkedIn} height= '100px' width = '100px' style={{borderRadius:'20%'}} alt = "linkedIn"></img>
                 </a>
             </motion.div>
             <motion.div style={{padding:'1em'}} >
                 <a href = "https://github.com/sergio-ayala123">
 
-                <img src = {githubImg} height= '100px' width = '100px' style={{borderRadius:'20%'}}></img>
+                <img src = {githubImg} height= '100px' width = '100px' style={{borderRadius:'20%'}} alt = "github"></img>
                 </a>
             </motion.div>
           </div>
